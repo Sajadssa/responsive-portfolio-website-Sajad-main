b3fe9d3518a1c72305bdfd748b5afff2c61cc4b9
sajadssa <sajadsaeediazad0007@gmail.com>
2022-08-25 14:29:36 +0430

    new changes had update

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,19 +93,40 @@ modalCloses.forEach((modalClose) => {
     });
 });
 /*==================== PORTFOLIO SWIPER  ====================*/
-// let swiper = new swiper(".portfolio__container", {
-//     slidesPerView: 1,
-//     spaceBetween: 30,
-//     loop: true,
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true
-//     },
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev"
-//     }
-// });
+const swiper = new Swiper('.swiper', {
+    loop: true, // creates a infinite loop of slides
+    navigation: { // activate navigation with navigation buttons
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {// activate paginations
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    },
+
+    // keyboard and mousewheel navigation
+    keyboard: true,
+    mousewheel: true,
+
+    // for other than sliding effect
+
+    // effect: 'creative',
+    // creativeEffect: {
+    //  prev: {
+    //      // will set `translateZ(-400px)` on previous slides
+    //      translate: [0, 0, -400],
+    //  },
+    //  next: {
+    //      // will set `translateX(100%)` on next slides
+    //      translate: ['100%', 0, 0],
+    //  },
+    // },
+    // effect: 'coverflow',
+});
+
+
+
 /*==================== TESTIMONIAL ====================*/
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
